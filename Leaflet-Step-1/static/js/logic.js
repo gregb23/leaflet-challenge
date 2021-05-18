f2d6b1fa6c6e2bf21ccb83840fa6f0bd3bf518d4
Gregory Brown <gregory.brown23@gmail.com>
2021-05-18 19:37:35 -0400

    set up link for geoJson

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -3,3 +3,21 @@ var myMap = L.map("map", {
     center: [38.5816, -121.4944],
     zoom: 6,
 });
+
+//create tile layer
+L.titleLayer(
+    "https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+    {
+        attribution:
+      'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: "light-v10",
+    accessToken: API_KEY,
+    }
+
+).addTo(myMap);
+
+// link from geoJson data (M4.5+ Earthquakes from past 7 days)
+var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
+
+//use d3 to get and view data
