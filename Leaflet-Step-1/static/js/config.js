ad0f18a377fc82792bd05fc9ebaf0106e3255c6b
Gregory Brown <gregory.brown23@gmail.com>
2021-05-18 19:30:02 -0400

    set up api key and base map

--- Leaflet-Step-1/static/js/config.js
+++ Leaflet-Step-1/static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "YOUR API KEY HERE!";
+const API_KEY = "pk.eyJ1IjoiZ3JlZ2IyMyIsImEiOiJja25icnR3MWYxdGRsMndtbDUwM2NiZ2ppIn0.XfUM3GXykBM-PX4MWnbGmA";
---
+++ Leaflet-Step-1/static/js/logic.js
@@ -0,0 +1,5 @@
+//set up map 
+var myMap = L.map("map", {
+    center: [38.5816, -121.4944],
+    zoom: 6,
+});
